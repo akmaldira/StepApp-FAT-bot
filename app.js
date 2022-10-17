6071de6b1a0883aebfe5dd679702b708cb6f1d83
akmaldira <akmaldiraa@gmail.com>
2022-10-17 17:53:35 +0700

    Add proxy

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const axios = require('axios');
 const readline = require('readline');
 const fs = require('fs');
+const events = require('events');
+const { SocksProxyAgent } = require('socks-proxy-agent');
 const { uniqueNamesGenerator, Config, names } = require('unique-names-generator');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 
-const sendOtp = async (email) => {
-    return await axios.get(`https://api.step.app/v1/auth/otp-code?email=${email}`)
-    .then(res => res.data)
-    .catch(err => console.error(err))
-    
+var logger = fs.createWriteStream('result.txt', {
+    flags: 'a'
+});
+
+const sendOtp = async (email, proxy) => {
+    if (proxy !== undefined) {
+        const proxyOptions = `socks5://${proxy}`;
+        const httpsAgent = new SocksProxyAgent(proxyOptions);
+        const client = axios.create({httpsAgent});
+        return await client.get(`https://api.step.app/v1/auth/otp-code?email=${email}`)
+        .then(res => res.data)
+        .catch(err => err.message)
+    } else {
+        return await axios.get(`https://api.step.app/v1/auth/otp-code?email=${email}`)
+        .then(res => res.data)
+        .catch(err => err.message)
+    }
 }
 
 const getMessageId = async (name, domain) => {
@@ -50,71 +64,74 @@ const verifyReff = async (accessToken, refferal) => {
 }
 
 
-const main = async (refferal) => {
-    const name = uniqueNamesGenerator({
-        dictionaries: [names],
-        style: 'lowerCase'
-    });
-    const domain = 'qiott.com';
-    console.log(name + '@' + domain);
-    const otp = await sendOtp(name + '@' + domain)
+var pointer = 0;
+const proxys = async () => {
 
-    if (otp === 'OK') {
-        console.log('OTP Sent');
-        const sleep = (milliseconds) => {
-            const date = Date.now();
-            let currentDate = null;
-            do {
-              currentDate = Date.now();
-            } while (currentDate - date < milliseconds);
-        };
-        console.log('Checking email verification...');
-        while (true) {
-            sleep(3000)
-            const message = await getMessageId(name, domain)
-            if (message.length > 0) {
-                if (message[0].subject === 'Email verification') {
-                    const messageBody = await getMessageBody(name, domain, message[0].id)
-                    const otp = messageBody.body.split('code: ')[1]
-                    const verify = await verifyEmail(name + '@' + domain, otp);
-                    const accessToken = verify.access.token
-                    
-                    const reff = await verifyReff(accessToken, refferal)
-                    if (reff === 'OK') {
-                        console.log(`Success add refferal to refferalId ${refferal} \n`);
-                        return name + '@' + domain
-                    } else {
-                        console.log('Error while verify refferal \n');
-                        return null
+    return fs.readFileSync('proxy.txt')
+        .toString('UTF8')
+        .split(/\r?\n/)[pointer];
+}
+
+const main = async(refferal, x) => {
+    var loop = parseInt(x) - 1;
+    for (let i = 0; i <= loop; i++) {
+        const name = uniqueNamesGenerator({
+            dictionaries: [names],
+            style: 'lowerCase'
+        });
+        const domain = 'qiott.com';
+        console.log(name + '@' + domain);
+
+        var proxy;
+
+        const otp = await sendOtp(name + '@' + domain, proxy);
+        if (otp === 'OK') {
+            console.log('OTP Sent');
+            const sleep = (milliseconds) => {
+                const date = Date.now();
+                let currentDate = null;
+                do {
+                  currentDate = Date.now();
+                } while (currentDate - date < milliseconds);
+            };
+
+            while (true) {
+                sleep(3000)
+                const message = await getMessageId(name, domain)
+                if (message.length > 0) {
+                    if (message[0].subject === 'Email verification') {
+                        const messageBody = await getMessageBody(name, domain, message[0].id)
+                        const otp = messageBody.body.split('code: ')[1]
+                        const verify = await verifyEmail(name + '@' + domain, otp);
+                        const accessToken = verify.access.token
+                        
+                        const reff = await verifyReff(accessToken, refferal)
+                        if (reff === 'OK') {
+                            logger.write(name + '@' + domain + '\n');
+                            console.log(`Success add refferal to refferalId ${refferal} \n`);
+                            break;
+                        } else {
+                            console.log('Error while verify refferal \n');
+                            break;
+                        }
                     }
                 }
+                console.log('OTP not found, waiting 3 sec');
             }
-            console.log('OTP not found, waiting 3 sec');
+        } else {
+            console.log(otp);
+            console.log('Register limit / bad proxy, try new proxy...\n');
+            proxy = await proxys()
+            console.log(`Using proxy ${proxy}...`);
+            pointer ++
+            i--
         }
-    } else {
-        console.log('Error sending OTP');
     }
 }
 
-const ask = msg => new Promise(resolve => 
-    rl.question(msg, response => resolve(response))
-);
-
-var logger = fs.createWriteStream('result.txt', {
-    flags: 'a'
-})
-
-rl.question('Refferal : ', function (refferal) {
-    rl.question('How much : ', async function (x) {
-        var loop = parseInt(x) - 1;
-        for (let i = 0; i <= loop; i++) {
-            await main(refferal)
-            .then((res) => {
-                if (res !== null) {
-                    logger.write(res+'\n')
-                }
-            })
-        }
+rl.question('Refferal : ', (reff) => {
+    rl.question('How much : ', (loop) => {
+        main(reff, loop)
         rl.close();
-    });
-});
+    })
+})
